39027e3f0adc38408ef347caea65a68e3985ce23
JavaTech <nonghoangvu2004@gmail.com>
2024-12-19 14:22:15 +0700

    init project

---
+++ src/test/java/javatech/vunh/JsonWebTokenApplicationTests.java
@@ -0,0 +1,13 @@
+package javatech.vunh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonWebTokenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
